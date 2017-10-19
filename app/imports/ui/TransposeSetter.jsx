5db980b756a9f445030dea3ee42885c7eef61319
Moritz Vifian <moritz.vifian@gmail.com>
2017-10-19 09:34:32 +0200

    Adding Common bezeichnung intervalle

--- app/imports/ui/TransposeSetter.jsx
+++ app/imports/ui/TransposeSetter.jsx
@@ -23,12 +23,35 @@ export default class TranposeSetter extends Component {
     // TODO: make object and calculate resulting key
     const options = [];
     const selection = 2; //convertthis.props.selection;
-    for (var i = -8; i <= 8; i++) {
+    let intervalls = new Map([
+      [0, "Nulline"],
+      [1, "Kl. Sekunde"],
+      [2, "Gr. Sekunde"],
+      [3, "Kl. Terz"],
+      [4, "Gr. Terz"],
+      [5, "Quart"],
+      [6, "Tritonus"],
+      [7, "Quint"],
+      [8, "Kl. Sext"],
+      [9, "Gr. Sext"],
+      [10, "Kl. Sept"],
+      [11, "Gr. Sept"],
+      [12, "Oktave "],
+      [13, "Kl. None"],
+      [14, "Gr. None"],
+      [15, "Kl. Dezime"],
+      [16, "Gr. Dezime"],
+      [17, "Kl. Undezime"]
+    ]);
+
+    for (var i = -17; i <= 17; i++) {
       var selected = false;
       if (i === selection) {
         const selected = true;
       }
-      options.push({ key: i,  });
+      let vz = (i<0?'-':'+');
+      let desc = vz+Math.abs(i)+' / '+vz+intervalls.get(Math.abs(i));
+      options.push({ key: i , desc: desc});
     }
     return (
       <div name="transposer">
