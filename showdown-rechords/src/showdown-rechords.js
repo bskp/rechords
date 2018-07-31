6ee13fcab6efba8fd47c1a3cf3c34a5e138ce8b3
Moritz Vifian <moritz.vifian@gmail.com>
2018-07-31 16:19:56 +0200

    adding underscore for empty syllables
    todo: regex that catches everything but [] and whitespace
    -> problem: intersection is not supported in javascript regex

--- showdown-rechords/src/showdown-rechords.js
+++ showdown-rechords/src/showdown-rechords.js
@@ -6,7 +6,7 @@ module.exports = function showdownRechords() {
   function parseLine(match, content) {
     // Only match chords followed by a letter or '.'
     // these chords are attached to the letter
-    var line = content.replace(/\[([^[\]]*?)\](?=[\w\.])/gi, '</span><span><i class="i">$1 </i>');
+    var line = content.replace(/\[([^[\]]*?)\](?=[\w\.äöüÄÖÜ])/gi, '</span><span><i class="i">$1 </i>');
     // the rest of the chords will be a different class
     var clazz;
     var mmatch = (/^( *\[([^\[\]]*?\]) *)+$/gi).test(line);
@@ -15,7 +15,7 @@ module.exports = function showdownRechords() {
     } else {
       clazz = "r";
     }
-    line = line.replace(/\[(.*?)\]/gi, '</span><span><i class="'+clazz+'">$1 </i>');
+    line = line.replace(/\[(.*?)\]/gi, '</span><span><i class="'+clazz+'">$1 </i>_');
     console.debug(line);
     return '<div class="line"><span>' + line + '</span></div>\n'; // line is allowed to be empty.
   }
