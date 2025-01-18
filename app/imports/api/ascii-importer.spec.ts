f373706c00d7559f30cde21291c91efe3852a0e9
Moritz Vifian <moritz.vifian@gmx.ch>
2025-01-18 16:44:06 +0100

    Testcases for ASCII converter

---
+++ app/imports/api/ascii-importer.spec.ts
@@ -0,0 +1,26 @@
+import fs from "fs";
+import path from "path";
+import assert from "assert";
+import { parseRechordsDown } from "./parseRechordsDown";
+import {describe, it} from 'node:test'
+import { convertToHoelibuSyntax } from "./ascii-importer";
+
+
+const cases = fs
+  .readdirSync(path.join(__dirname, "ascii-cases"))
+  .filter((f) => f.endsWith(".txt"))
+  .map((f) => path.join(__dirname, "ascii-cases", f));
+
+const ascii = describe("ascii-2-showdown", () => {
+  cases.forEach((inPath) => {
+    it("Case: " + inPath.replace(/^.*\//, ""), () => {
+      const ascii = fs.readFileSync(inPath, "utf8");
+      const actual = convertToHoelibuSyntax(ascii)
+      const expectedMarkdown = fs.readFileSync(inPath.replace(/.txt$/, ".md"), "utf8");
+
+      assert.strictEqual(actual, expectedMarkdown);
+    });
+  });
+});
+
+export { ascii};
---
+++ app/imports/tests.spec.ts
@@ -0,0 +1,5 @@
+import { showdown } from "./api/showdown-rechords.spec";
+import { ascii } from "./api/ascii-importer.spec";
+
+showdown;
+ascii;
