3dfb2162cd84288e296193e9bdd7388e388a80cf
Moritz Vifian <moritz.vifian@gmx.ch>
2026-04-08 07:34:29 +0200

    do not pipe the out

--- app/playwright.config.ts
+++ app/playwright.config.ts
@@ -13,8 +13,6 @@ export default defineConfig({
     command: "meteor --settings settings.json --exclude-archs web.browser.legacy --port 3000",
     url: "http://localhost:3000",
     reuseExistingServer: !process.env.CI,
-    timeout: 120000,
-    stdout: "pipe",
-    stderr: "pipe",
+    timeout: 300000,
   },
 });
